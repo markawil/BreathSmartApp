c04b6346b8420cc12b4f8a4d411f9a1f569b813a
Mark Wilkinson <mark.wilkinson@velentium.com>
2024-12-13 12:56:30 -0600

    Initial Commit

---
+++ BreathSmartCBL/BreathSmartCBLApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreathSmartCBLApp.swift
+//  BreathSmartCBL
+//
+//  Created by MarkWilkinson on 12/13/24.
+//
+
+import SwiftUI
+
+@main
+struct BreathSmartCBLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreathSmartCBLTests/BreathSmartCBLTests.swift
@@ -0,0 +1,17 @@
+//
+//  BreathSmartCBLTests.swift
+//  BreathSmartCBLTests
+//
+//  Created by MarkWilkinson on 12/13/24.
+//
+
+import Testing
+@testable import BreathSmartCBL
+
+struct BreathSmartCBLTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
